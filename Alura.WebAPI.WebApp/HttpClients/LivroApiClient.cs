088da2ee9b1fe699a5de1d5e7a73b7cddde7c339
devfabiorm <fabio.evangelion.guitar@gmail.com>
2021-02-20 03:48:33 -0300

    Usando a Factory do HttpClient para fabricar o objeto e classe para consumo da API

---
+++ Alura.WebAPI.WebApp/HttpClients/LivroApiClient.cs
@@ -0,0 +1,35 @@
+﻿using Alura.ListaLeitura.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.HttpClients
+{
+    public class LivroApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public LivroApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<byte[]> GetLivroCapaAsync(int id)
+        {
+            HttpResponseMessage resposta = await _httpClient.GetAsync($"livros/capas/{id}");
+            resposta.EnsureSuccessStatusCode();
+
+            return await resposta.Content.ReadAsByteArrayAsync();
+        }
+
+        public async Task<LivroApi> GetLivroAsync(int id)
+        {
+            HttpResponseMessage resposta = await _httpClient.GetAsync($"livros/{id}");
+            resposta.EnsureSuccessStatusCode();
+
+            return await resposta.Content.ReadAsAsync<LivroApi>();
+        }
+    }
+}
