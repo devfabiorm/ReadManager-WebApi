70672eeef9ead8ac28732972e94f36fee06d24f7
devfabiorm <fabio.evangelion.guitar@gmail.com>
2021-02-22 23:50:25 -0300

    Versionamento através do Headers de requisição

--- Alura.WebAPI.Api/Startup.cs
+++ Alura.WebAPI.Api/Startup.cs
@@ -4,6 +4,7 @@ using Alura.ListaLeitura.Persistencia;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -53,7 +54,9 @@ namespace Alura.WebAPI.Api
                 };
             });
 
-            services.AddApiVersioning();
+            services.AddApiVersioning(options => {
+                options.ApiVersionReader = new HeaderApiVersionReader("api-version");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
