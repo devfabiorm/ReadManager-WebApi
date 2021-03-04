8c95dfaa6c45c0abd3957d98198a923518a294a5
devfabiorm <fabio.evangelion.guitar@gmail.com>
2021-03-03 23:53:10 -0300

    Criando modelos de paginacao, modelo de retorno e método de estensão para gerar retorno novo tipo

--- Alura.WebAPI.Api/Controllers/Livros2Controller.cs
+++ Alura.WebAPI.Api/Controllers/Livros2Controller.cs
@@ -21,13 +21,16 @@ namespace Alura.ListaLeitura.Api.Controllers
         }
 
         [HttpGet]
-        public IActionResult ListaDeLivros([FromQuery] LivroFiltro filtro, [FromQuery] LivroOrdem ordem)
+        public IActionResult ListaDeLivros(
+            [FromQuery] LivroFiltro filtro,
+            [FromQuery] LivroOrdem ordem,
+            [FromQuery] LivroPaginacao paginacao)
         {
             var livros = _repo.All
                 .AplicaFiltro(filtro)
                 .AplicaOrdem(ordem)
                 .Select(l => l.ToApi())
-                .ToList();
+                .ToLivroPaginado(paginacao);
             return Ok(livros);
         }
 
